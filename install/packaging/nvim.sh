14f92711b78cf94fefb79dbe0b1a5142661c1a91
Mario René Estrada Sobalvarro <43856347+Woland1993@users.noreply.github.com>
2026-04-21 21:49:00 -0600

    Fix nvim setup - use LazyVim starter directly

--- install/packaging/nvim.sh
+++ install/packaging/nvim.sh
@@ -1,2 +1,5 @@
-# Includes lazyvim and the themes
-aletheia-nvim-setup
+# Setup neovim with LazyVim
+if [[ ! -d ~/.config/nvim ]]; then
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+  rm -rf ~/.config/nvim/.git
+fi
